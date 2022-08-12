259ecf2f7cd9dee7e0947bacd9ebe6b581199bb3
Rowan Backhouse <rowanbackhouse@live.com>
2022-08-11 20:14:40 -0400

    updated logic.js to include linestrings, coloured them

--- Mapping_GeoJSON_Points/Static/js/logic.js
+++ Mapping_GeoJSON_Points/Static/js/logic.js
@@ -1,6 +1,6 @@
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -15,27 +15,40 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Street: light,
     Dark: dark
 };
 
 
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/roborowanb/Mapping_earthquakes/main/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/roborowanb/Mapping_earthquakes/main/torontoRoutes.json";
 
 
-d3.json(airportData).then(function(data) {
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
+
+d3.json(torontoData).then(function(data) {
     console.log(data);
-    L.geoJSON(data).addTo(map);
+    L.geoJSON(data, {
+        style: myStyle,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination: "
+            + feature.properties.dst + "</h3>");
+        }
+    })
+    .addTo(map);
 });
 
